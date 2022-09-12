e6afbbf7be09b94c84b7d54d25f9cd3b9a754d9d
EbenJr <112567051+EbenJr@users.noreply.github.com>
2022-09-12 11:28:31 +0700

    Add files via upload

---
+++ Latian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"MENGHITUNG LUAS LINGKARAN"<<endl;
+	cout<<"Pi = 3,14"<<endl;
+	float pi = 3.14;
+	int r;
+	cout<<"Masukkan Angka : ";
+	cin>>r;
+	
+	cout<<"Hasil Luas Lingkaran : ";
+	cout<< pi*r*r;
+	return 0;
+}
